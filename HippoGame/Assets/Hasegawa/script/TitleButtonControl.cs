1cb253335037be815b3c9be0934586f99ebbcb63
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-12-04 23:10:44 +0900

    タイトルのボタン制御、プレイヤーの左右移動を実装

---
+++ HippoGame/Assets/Hasegawa/script/TitleButtonControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleButtonControl : MonoBehaviour {
+
+	void Awake() {
+		GameObject.Find ("StartButton").GetComponent<Button> ().onClick.AddListener (() => OnClickStartButton ());
+		GameObject.Find ("ExitButton").GetComponent<Button> ().onClick.AddListener (() => OnClickExitButton ());
+	}
+
+	public void OnClickStartButton(){
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void OnClickExitButton(){
+		Application.Quit ();
+	}
+
+}
