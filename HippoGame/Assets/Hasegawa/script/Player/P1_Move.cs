2531a8d13dda969a4d233e6e44b5a6f0115b3266
unknown <nwuser@GM2150026.da.iwasaki.ac.jp>
2017-12-13 10:34:15 +0900

    楳内：playerの移動、当たり判定、移動補正完了

--- HippoGame/Assets/Hasegawa/script/Player/P1_Move.cs
+++ HippoGame/Assets/Hasegawa/script/Player/P1_Move.cs
@@ -1,35 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class P1_Move : MonoBehaviour {
 
 	// 速度
-	[SerializeField]float movespd = .2f;
+	//[SerializeField]float movespd = .2f;
 	// Transform格納
-	Transform trans;
+	//Transform trans;
 
 	/// <summary>
 	/// 初期化
 	/// </summary>
 	void Start () {
-		trans = transform;
+	//	trans = transform;
 	}
 
-	/// <summary>
-	/// メインループ
-	/// </summary>
-	void Update () {
-		Move ();
-	}
+    /// <summary>
+    /// メインループ
+    /// </summary>
+    void Update()
+    {
+        //Move ();
+        if (transform.position.x < -3.5 )
+        {
+            transform.position = new Vector3(-3.5f,this.transform.position.y,this.transform.position.z);
+        }
+        if (transform.position.x > 3.5)
+        {
+            transform.position = new Vector3(3.5f, this.transform.position.y, this.transform.position.z);
+        }
+        float dx = Input.GetAxis("Horizontal");
+            transform.Translate(dx / 2, 0, 0);
+        
+    }
+        void OnCollisionStay(Collision other)
+    {
+            if (other.gameObject.tag == "enemy")
+            {
+            SceneManager.LoadScene("Title");
+                Debug.Log("Hit");
+            }
+        }
 
-	/// <summary>
-	/// 左右移動
-	/// </summary>
-	void Move(){
+        /// <summary>
+        /// 左右移動
+        /// </summary>
+        /*void Move(){
 		if (Input.GetKey (KeyCode.LeftArrow))
 			trans.position -= trans.right * movespd;
 		if (Input.GetKey (KeyCode.RightArrow))
 			trans.position += trans.right * movespd;
-	}
+	}*/
 }
