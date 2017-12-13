6b3d7742f3fd61cbed6e8c512ac45f04bb4db77e
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-12-13 17:43:05 +0900

    E3_Sweat・E4_Puddle・E5_Rockの実装

---
+++ HippoGame/Assets/Hasegawa/script/Enemy/E4_Puddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E4_Puddle : MonoBehaviour {
+
+	[SerializeField] float puddlespd = .2f;
+	E1_Move move;
+
+	void Start () {
+		move = GetComponent<E1_Move> ();
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.tag == "Puddle")
+			Puddle (true);
+	}
+
+	void OnTriggerExit(Collider collider){
+		if (collider.tag == "Puddle")
+			Puddle (false);
+	}
+
+	void Puddle(bool flg){
+		move.Speed += (flg) ? puddlespd : -puddlespd;
+	}
+}
