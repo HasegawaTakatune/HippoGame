75cf7d0b71c893f58096e8d38e980b17f748e8f1
DA\nwuser <0ta5274738t4p4r@gmail.com>
2018-01-10 13:25:23 +0900

    敵がターゲットに突進をする・岩を回避する処理を追加

--- HippoGame/Assets/Hasegawa/script/Enemy/myConst.cs
+++ HippoGame/Assets/Hasegawa/script/Enemy/myConst.cs
@@ -9,3 +9,7 @@ public class myConst : MonoBehaviour {
 public static class STATUS{
 	public const byte IDLE = 0,STR = 1;
 }
+
+public static class Game{
+	public const byte LEVEL1 = 1,LEVEL2 = 2,LEVEL3 = 3,LEVEL4 = 4,LEVEL5 = 5;
+}
