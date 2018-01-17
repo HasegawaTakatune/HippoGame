471af64466f4929a4b33e80b53cd5d6f9395b512
DA\nwuser <xkira42x@gmail.com>
2018-01-17 11:36:54 +0900

    スカイボックスを適用 ステージのスライドをループを追加 各オブジェクトの位置を調整

--- HippoGame/Assets/Aikei/Scripts/G7_Slide.cs
+++ HippoGame/Assets/Aikei/Scripts/G7_Slide.cs
@@ -9,6 +9,15 @@ public class G7_Slide : MonoBehaviour {
 	Transform trans;	//使うTransformを保存する変数宣言
 	G1_GameManager GM;	//使うゲームマネージャを保存する変数宣言
 
+	[SerializeField]
+	bool loopflg=false;
+	[SerializeField]
+	Transform loopZobject;	//ループするオブジェクトのスケールを取得
+
+	float initpos;		//このオブジェクトの初期座標
+	float loopshiftpos;	//ループされた座標
+	[SerializeField]
+	byte loopobjcts;	//ループするオブジェクト数
 	// Use this for initialization
 	void Start () {
 		//このオブジェクトのスライド速度をGameManagerから参照する
@@ -16,6 +25,13 @@ public class G7_Slide : MonoBehaviour {
 
 		//このオブジェクトのTransformを格納する
 		trans = this.gameObject.transform;
+
+		if (loopZobject != null) {
+			initpos = loopZobject.position.z;
+			if (initpos < 0)
+				initpos *= -1;
+			loopshiftpos = initpos * loopZobject.localScale.z / 10;
+		}
 	}
 	
 	// Update is called once per frame
@@ -28,5 +44,17 @@ public class G7_Slide : MonoBehaviour {
 	/// </summary>
 	void Slide(){
 		trans.position -= new Vector3 (0, 0, slidespd);
+		if (loopflg == true) Loop ();
+	}
+	/// <summary>
+	/// オブジェクトをループする関数
+	/// </summary>
+	void Loop(){
+		//現在の座標　よりも　初期座標ーループ後の座標が大きいならば
+		if (trans.position.z <= -initpos-loopshiftpos) {
+			Debug.Log (trans.name+":loop");
+			trans.position += new Vector3 (0, 0, loopshiftpos*loopobjcts);
+		}
+
 	}
 }
