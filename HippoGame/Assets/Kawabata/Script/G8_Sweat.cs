046deaf958c314640b00b4d886b28dffa78a1242
FeyCiel <n5373d7fd13t@gmail.com>
2018-02-04 16:40:09 +0900

    体液が地面に触れた時小さい水溜りを生成　生成した水溜りを時間で拡大するスクリプトの追加

---
+++ HippoGame/Assets/Kawabata/Script/G8_Sweat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G8_Sweat : MonoBehaviour {
+
+	[SerializeField]
+	GameObject SweatObject=null; //水たまりのオブジェクト
+
+	private void OnCollisionEnter(Collision col){
+		if (col.gameObject.CompareTag ("Floor")) {   //"Floor"のタグにぶつかったら
+			SweatSpread (col.gameObject);			 //SweatSpread()関数に飛ぶ
+
+
+		}
+	}
+
+	private void SweatSpread(GameObject floor){
+		Vector3 sweat_pos = this.transform.position;  //Floorに触れた時の体液の座標取得
+		Instantiate (SweatObject, sweat_pos, Quaternion.identity);　//(水溜りのオブジェクト,体液の座標,回転無し)
+		Destroy (this.gameObject);						//体液の削除
+	}
+
+}
---
+++ HippoGame/Assets/Kawabata/Script/G9_SpreadSweat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G9_SpreadSweat : MonoBehaviour {
+
+	[SerializeField]
+	float SweatRange = 0;   //体液の横幅
+	[SerializeField]
+	float SweatSpreadTime = 0;  //体液が広がる速さ(大きいほど遅い)
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Spread", SweatSpreadTime);	
+	}
+	
+	private  IEnumerator Spread(float Time){
+		while (this.transform.localScale.x < SweatRange) { //Scale.xの数値がSweatRangeより小さいなら
+			yield return new WaitForSeconds (Time);        
+			this.transform.localScale += new Vector3 (0.1f, 0, 0);   //0.1ずつ横に伸ばす
+		}
+
+	}
+}
